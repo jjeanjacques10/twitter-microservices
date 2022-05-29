3dc6113bdbad383f0adf1bb50578c38cd58cac01
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-05-29 01:52:11 -0300

    fix tweet dynamodb

--- tweet-service/src/main/java/com/twitter/tweetservice/config/DynamoDbConfig.java
+++ tweet-service/src/main/java/com/twitter/tweetservice/config/DynamoDbConfig.java
@@ -1,20 +1,33 @@
 package com.twitter.tweetservice.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.net.URI;
 
+@Slf4j
 @Configuration
 public class DynamoDbConfig {
+
+    @Value("${cloud.aws.dynamodb.url}")
+    private String dynamoDbUrl;
+
     @Bean
+    @Primary
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public DynamoDbClient dynamoDbClient() {
+        log.info("Config AWS DynamoDB - URL: {}", dynamoDbUrl);
         return DynamoDbClient.builder()
                 .region(Region.US_WEST_1)
-                .endpointOverride(URI.create("http://localhost:4566"))
+                .endpointOverride(URI.create(dynamoDbUrl))
                 .build();
     }
 
