44ae4a042f748329fbb618c6f43272da9c0050d3
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-06-02 20:31:35 -0300

    add heroku config
    add heroku config
    
    change port gateway
    
    gateway actuator
    
    gateway actuator
    
    update procfile
    
    clean to run offline
    
    update timeline heroku
    
    update timeline heroku
    
    update gateway
    
    update gateway
    
    update gateway
    
    update procfile
    
    update procfile
    
    update procfile
    
    update tweet
    
    update tweet
    
    update tweet
    
    update tweet
    
    update tweet database
    
    update README.md

--- timeline-service/src/main/kotlin/com/twitter/config/RedisConfig.kt
+++ timeline-service/src/main/kotlin/com/twitter/config/RedisConfig.kt
@@ -1,8 +1,10 @@
 package com.twitter.config
 
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration.LettuceClientConfigurationBuilder
 import org.springframework.data.redis.core.RedisTemplate
 import java.awt.print.Book
 
@@ -15,4 +17,14 @@ class RedisConfig {
         template.setConnectionFactory(connectionFactory!!)
         return template
     }
+
+    @Bean
+    fun lettuceClientConfigurationBuilderCustomizer(): LettuceClientConfigurationBuilderCustomizer? {
+        return LettuceClientConfigurationBuilderCustomizer { clientConfigurationBuilder: LettuceClientConfigurationBuilder ->
+            if (clientConfigurationBuilder.build().isUseSsl) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification()
+            }
+        }
+    }
+
 }
--- tweet-service/src/main/java/com/twitter/tweetservice/config/SQSLocalConfig.java
+++ tweet-service/src/main/java/com/twitter/tweetservice/config/SQSLocalConfig.java
@@ -1,7 +1,6 @@
 package com.twitter.tweetservice.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
@@ -10,28 +9,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import software.amazon.awssdk.regions.Region;
 
+@Profile("local")
 @Slf4j
 @Configuration
-public class SQSConfig {
+public class SQSLocalConfig {
 
-    @Value("${cloud.aws.accessKey}")
-    private String accessKey;
+    @Value("${cloud.aws.sqs.url}")
+    private String sqsUrl;
 
-    @Value("${cloud.aws.secretKey}")
-    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String sqsRegion;
 
     @Bean
     @Primary
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public AmazonSQSAsync amazonSQSAsync() {
-        log.info("Config AWS SQS - URL: {} Region: {}", accessKey, secretKey); // TODO: Remove it
+        log.info("Config AWS SQS - URL: {} Region: {}", sqsUrl, sqsRegion);
         return AmazonSQSAsyncClientBuilder.standard()
-                .withRegion(Region.US_WEST_1.toString())
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(sqsUrl, sqsRegion))
                 .build();
     }
 
